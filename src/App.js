435c73efe35880eada8f5c24ff8781b10cd48951
lee byeonghoon <qudgns200@gmail.com>
2019-12-26 21:25:42 +0900

    Study react(20191226)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types"
+
+function Food({name, image, rating}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <h2> {image} </h2>
+  </div>;
+}
+
+const fav = [
+  {
+    id : 1,
+    name : "kimchi",
+    image : "CCCC",
+  },
+  {
+    id : 2,
+    name : "gogi",
+    image : "DDDD",
+  }
+];
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {fav.map(dish => (
+        <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating} />
+        ))
+      }
     </div>
-  );
+    );
 }
 
 export default App;
